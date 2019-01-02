c0695175d0e2c0806435e8c67eb1990a877aa079
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-02 14:39:01 +0100

    c'est bon ça marche :)

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,1 @@
-function popup()
-{
- //Traitement
- setTimeOut(alert, 10000); //On attend 10 secondes avant d'exécuter la fonction
-}
-window.alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-{
- //Le code écrit dans cette fonction ne sera exécuté qu'au bout de 10 secondes
-}
+setTimeout(function popup() {alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');}, 10000);
