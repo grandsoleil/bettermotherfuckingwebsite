3ee0352bfd6aecebfe54ae5e03d858fa74cf4199
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-02 14:31:50 +0100

    set timeout

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
+function popup()
+{
+ //Traitement
+ setTimeOut(alert, 10000); //On attend 10 secondes avant d'exécuter la fonction
+}
 window.alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-setTimeout(alert, 10000);
+{
+ //Le code écrit dans cette fonction ne sera exécuté qu'au bout de 10 secondes
+}
