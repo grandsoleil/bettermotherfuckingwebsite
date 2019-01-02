9e3e4bd6656cbef49abc2d0fd2234ec6bb4386e2
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-02 14:51:50 +0100

    marche toujours new test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-setTimeout(function popup() {alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');}, 10000);
+setTimeout(function () {alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');}, 10000);
