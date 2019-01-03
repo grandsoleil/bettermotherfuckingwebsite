11f0d378468f16e5676df95faef42fc27d8ca3a9
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-03 12:46:03 +0100

    remis window.location =

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ setTimeout(function () {
 var redir;
 var r = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
 if (r == true) {
-  redir = window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  redir = window.location = ('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 	}
 
 }, 10000);
