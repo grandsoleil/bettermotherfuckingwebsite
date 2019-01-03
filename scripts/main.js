9339e3a11a050bc0b8bbc291302508387138569c
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-03 12:20:33 +0100

    ajout fonction if a javascriot

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-setTimeout(function () {confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); window.location = ('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}, 10000);
+setTimeout(function () {
+
+var redir;
+var r = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if (r == true) {
+  redir = window.location = ('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+	}
+
+}, 10000);
